7d7a2fdafda24e18700f2165d956a52caa54fed6
nielsengelen <niels@foonet.be>
2021-04-02 10:29:04 +0200

    v5 release

--- includes/jobs.php
+++ includes/jobs.php
@@ -3,20 +3,21 @@ require_once('../config.php');
 require_once('../veeam.class.php');
 
 session_start();
+error_reporting(E_ALL || E_STRICT);
+
+$veeam = new VBO($host, $port, $version);
 
 if (isset($_SESSION['token'])) {
-	$veeam = new VBO($host, $port, $version);
 	$veeam->setToken($_SESSION['token']);
     $user = $_SESSION['user'];
 	$jobs = $veeam->getJobs();
 	$org = $veeam->getOrganizations();
-	$proxies = $veeam->getProxies();
 ?>
 <div class="main-container">
     <h1>Jobs</h1>
     <?php
-    if (count($jobs) != '0') {
-    ?>
+    if (count($jobs) !== 0) {
+	?>
 	<input class="form-control search" id="filter-jobs" placeholder="Filter jobs..." />
     <table class="table table-hover table-bordered table-striped table-border" id="table-jobs">
         <thead>
@@ -24,7 +25,8 @@ if (isset($_SESSION['token'])) {
                 <th>Job Name</th>
 				<th>Organization</th>
                 <th>Status</th>
-                <th>Next Run</th>
+				<th>Next Run</th>
+                <th>Description</th>
                 <th class="text-center">Schedule</th>
                 <th class="text-center">Restore Points</th>
                 <th class="text-center">Options</th>
@@ -46,11 +48,12 @@ if (isset($_SESSION['token'])) {
 
 			echo '<td>' . (isset($jobs[$i]['lastRun']) ? $jobs[$i]['lastStatus'] . ' (' .  date('d/m/Y H:i T', strtotime($jobs[$i]['lastRun'])) . ')' : $jobs[$i]['lastStatus']) . '</td>';
 			echo '<td>' . (isset($jobs[$i]['nextRun']) ? date('d/m/Y H:i T', strtotime($jobs[$i]['nextRun'])) : 'Not scheduled') . '</td>';
-			echo '<td class="pointer text-center" data-toggle="collapse" data-target="#schedule'.$i.'"><a href="#" onClick="return false;">View</a></td>';
-			echo '<td class="pointer text-center" data-toggle="collapse" data-target="#restorepoints'.$i.'"><a href="#" onClick="return false;">View</a></td>';
+			echo '<td>' . $jobs[$i]['description'] . '</td>';
+			echo '<td class="pointer text-center" data-toggle="collapse" data-target="#schedule'.$i.'"><a href="#" onclick="return false;">View</a></td>';
+			echo '<td class="pointer text-center" data-toggle="collapse" data-target="#restorepoints'.$i.'"><a href="#" onclick="return false;">View</a></td>';
 			echo '<td>';
 			
-			if ($jobs[$i]['isEnabled'] != 'true') {
+			if ($jobs[$i]['isEnabled'] != '1' || empty($jobs[$i]['isEnabled'])) {
 				echo '<button class="btn btn-default btn-change-job-state" id="btn-change-job-state-' . $jobs[$i]['id'] . '" data-call="enable" data-name="' . $jobs[$i]['name'] . '" data-jid="' . $jobs[$i]['id'] . '" title="Change state"><i class="fa fa-power-off text-success fa-lg btn-state-' . $jobs[$i]['id'] . '"></i></button></a>';
 			} else {
 				echo '<button class="btn btn-default btn-change-job-state" id="btn-change-job-state-' . $jobs[$i]['id'] . '" data-call="disable" data-name="' . $jobs[$i]['name'] . '" data-jid="' . $jobs[$i]['id'] . '" title="Change state"><i class="fa fa-power-off text-danger fa-lg btn-state-' . $jobs[$i]['id'] . '"></i></button></a>';
@@ -60,7 +63,7 @@ if (isset($_SESSION['token'])) {
 			echo '</td>';
 			echo '</tr>';
 			
-			echo '<tr>'; /* Start of table for job schedule */
+			echo '<tr>';
 			echo '<td colspan="7" class="zeroPadding">';
 			echo '<div id="schedule'.$i.'" class="accordian-body collapse">';
 			?>
@@ -97,8 +100,9 @@ if (isset($_SESSION['token'])) {
 			echo '</td>';
 			echo '</tr>';
 			
-			echo '<tr>'; /* Start of table for job restore points */
-			echo '<td colspan="7" class="zeroPadding"><div id="restorepoints'.$i.'" class="accordian-body collapse">';
+			echo '<tr>';
+			echo '<td colspan="7" class="zeroPadding">';
+			echo '<div id="restorepoints'.$i.'" class="accordian-body collapse">';
 			?>
 				<table class="table table-bordered table-small table-striped">
 				<thead>
@@ -113,7 +117,11 @@ if (isset($_SESSION['token'])) {
 				<tbody>
 				<?php
 				$jobsession = $veeam->getJobSession($jobs[$i]['id']);
-
+				
+				if ($version === 'v5') {
+					$jobsession = $jobsession['results'];
+				}
+				
 				for ($j = 0; $j < count($jobsession); $j++) {
 					echo '<tr>';
 					echo '<td>' . (isset($jobsession[$j]['endTime']) ? date('d/m/Y H:i T', strtotime($jobsession[$j]['endTime'])) : 'N/A') . '</td>';
@@ -126,7 +134,7 @@ if (isset($_SESSION['token'])) {
 					}
 					echo '<td>' . $jobsession[$j]['statistics']['bottleneck'] . '</td>';
 					echo '<td>' . $jobsession[$j]['statistics']['processedObjects'] . ' items processed</td>';
-					echo '<td class="text-center"><a href="#" class="item" data-sessionid="' . $jobsession[$j]['id'] . '" onClick="return false;">View</a></td>';					
+					echo '<td class="text-center"><a href="#" class="item" data-sessionid="' . $jobsession[$j]['id'] . '" data-sessiontime="' . (isset($jobsession[$j]['endTime']) ? date('d/m/Y H:i T', strtotime($jobsession[$j]['endTime'])) : 'N/A') . '" onclick="return false;">View</a></td>';					
 					echo '</tr>';
 				}
 				?>
@@ -151,7 +159,7 @@ if (isset($_SESSION['token'])) {
     <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title">Session info</h1>
+        <h1 class="modal-title" id="session-title">Session info</h1>
       </div>
       <div class="modal-body">
         <table class="table table-bordered table-padding table-striped" id="table-session-content">
@@ -173,10 +181,9 @@ if (isset($_SESSION['token'])) {
 </div>
 
 <script>
-/* Job filter */
 $('#filter-jobs').keyup(function(e) {
     var searchText = $(this).val().toLowerCase();
-    /* Show only matching row, hide rest of them */
+    
     $.each($('#table-jobs tbody tr'), function(e) {
         if ($(this).text().toLowerCase().indexOf(searchText) === -1) {
            $(this).hide();
@@ -186,24 +193,26 @@ $('#filter-jobs').keyup(function(e) {
     });
 });
 
-/* Session window */
 $('.item').click(function(e) {
     var icon, text;
     var id = $(this).data('sessionid');
-    
+    var time = $(this).data('sessiontime');
+	
+	$('#session-title').text('Session info (' + time + ')');
+	
     $.post('veeam.php', {'action' : 'getbackupsessionlog', 'id' : id}).done(function(data) {
         response = JSON.parse(data);
 
         $('#table-session-content tbody').empty();
         
         for (var i = 0; i < response.results.length; i++) {
-            if (response.results[i].title.match(/Success/g)) { /* Success icon */
+            if (response.results[i].title.match(/Success/g)) {
                 icon = 'check-circle';
                 text = 'success';
-            } else if (response.results[i].title.match(/Warning/g)) { /* Warning icon */
+            } else if (response.results[i].title.match(/Warning/g)) {
                 icon = 'exclamation-triangle';
                 text = 'warning';
-            } else { /* Failed icon */
+            } else {
                 icon = 'times-circle';
                 text = 'danger';
             }
@@ -222,15 +231,14 @@ $('.item').click(function(e) {
     });
 });
 
-/* Job Buttons */
 $('.btn-change-job-state').click(function(e) {
-    var jid = $(this).data('jid'); /* Job ID */
-    var name = $(this).data('name'); /* Job name */
-    var call = $(this).data('call'); /* Job call: enable or disable */
+    var jid = $(this).data('jid');
+    var name = $(this).data('name');
+    var call = $(this).data('call');
     var json = '{ "'+call+'": null }';
 
     $.post('veeam.php', {'action' : 'changejobstate', 'id' : jid, 'json' : json}).done(function(data) {
-		if (call == 'enable') {
+		if (call === 'enable') {
 			$('#btn-change-job-state-'+jid).data('call', 'disable');
 			$('.btn-state-'+jid).removeClass('text-success');
 			$('.btn-state-'+jid).addClass('text-danger');
@@ -238,19 +246,33 @@ $('.btn-change-job-state').click(function(e) {
 			$('#btn-change-job-state-'+jid).data('call', 'enable');
 			$('.btn-state-'+jid).addClass('text-success');
 			$('.btn-state-'+jid).removeClass('text-danger');
-		}			
+		}
+		
+		Swal.fire({
+			icon: 'info',
+			title: 'Job state',
+			text: 'Job "' + name + '" has been ' + call + 'd'
+		})
 	});
 });
 
 $('.btn-job-start').click(function(e) {
-    var id = $(this).data('jid'); /* Job ID */
-    var name = $(this).data('name'); /* Job name */
+    var id = $(this).data('jid');
+    var name = $(this).data('name');
     
     $.post('veeam.php', {'action' : 'startjob', 'id' : id}).done(function(data) {
+		var response = JSON.parse(data);
+
+		if (response == null) {
+			var message = 'Job has been started';
+		} else {
+			var message = response['message'].slice(0, -1);
+		}
+		
 		Swal.fire({
-			type: 'info',
+			icon: 'info',
 			title: 'Job status',
-			text: '' + data
+			text: '' + message
 		})
     });
 });
@@ -263,16 +285,15 @@ $('.btn-job-start').click(function(e) {
 		$_SESSION['refreshtoken'] = $veeam->getRefreshToken();
         $_SESSION['token'] = $veeam->getToken();
 	} else {
-		unset($_SESSION);
-		session_destroy();
+		$veeam->logout();
 		?>
 		<script>
 		Swal.fire({
-			type: 'info',
+			icon: 'info',
 			title: 'Session expired',
-			text: 'Your session has expired and requires you to login again.'
+			html: 'Your session has expired and requires you to log in again',
 		}).then(function(e) {
-			window.location.href = '/index.php';
+			window.location.href = '/';
 		});
 		</script>
 		<?php
